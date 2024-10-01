544b560d1a1759a322c576a7e3b1d174fb7cffe0
Oleg Grynets <oleg.grinets@gmail.com>
2024-10-01 00:24:11 +0000

    Added nvidia

--- assitant_test.py
+++ assitant_test.py
@@ -9,6 +9,7 @@ from llama_index.core import Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.response.pprint_utils import pprint_response
+from llama_index.embeddings.ollama import OllamaEmbedding
 
 ollama_base_url=os.environ['OLLAMA_BASE_URL']
 pdf_images_path=os.environ['PDF_IMAGES_STORE_PATH']
@@ -33,20 +34,23 @@ ollama = Ollama(model="llama3.1:8b", request_timeout=120.0, base_url=ollama_base
                 is_function_calling_model=True)
 ollama_agent = Ollama(model="llama3.2:latest", request_timeout=120.0, base_url=ollama_base_url, 
                 is_function_calling_model=True)
+embed_model = OllamaEmbedding(model_name="bge-m3", request_timeout=120.0, base_url=ollama_base_url)
+
 
 hf_embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.llm = ollama
 Settings.embed_model = hf_embed_model
 
 agent = MultiDocumentAssistantAgentsPack(main_llm=ollama, agent_llm=ollama_agent, 
-                                         embeding_llm=hf_embed_model, 
+                                         embeding_llm=embed_model, 
                                          docs_store_path=docs_store_path, 
                                          pdf_images_path=pdf_images_path,
                                          storage_dir=storage_dir, verbose=True)
 #response = agent.run("Give me the location of Homewood Suites by Hilton. Do not guess only use context.")
 #response = agent.run("Give me the address of Homewood Suites by Hilton where I was staying and you have an invoce from this hotel.")
-#response = agent.run("How many times Oleg Grynets stayed in hotels?")
-response = agent.run("What genAI-enabled or -assisted experiences benefits will be delivered to enterprises according to Forester?")
+response = agent.run("What was the outcome of Akela's attack yesterday, as per the jungle's law?")
+response = agent.run("What action did not occur, instead of going to the store?")
+#response = agent.run("What genAI-enabled or -assisted experiences benefits will be delivered to enterprises according to Forester?")
 print(response)
 
 

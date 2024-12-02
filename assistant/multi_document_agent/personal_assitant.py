531e87dd8b3839af1d2bd0060c2fa5fe891a1fb0
Oleg Grynets <oleg.grinets@gmail.com>
2024-12-02 15:03:30 +0000

    Added intent identification logic.

--- assistant/multi_document_agent/personal_assitant.py
+++ assistant/multi_document_agent/personal_assitant.py
@@ -3,7 +3,7 @@
 from typing import Any, Dict, List
 
 from llama_index.core import Settings, SummaryIndex, VectorStoreIndex
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, get_response_synthesizer
 from llama_index.core.llama_pack.base import BaseLlamaPack
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.objects import ObjectIndex, SimpleToolNodeMapping
@@ -25,6 +25,10 @@ from llama_index.core import ChatPromptTemplate
 from llama_index.postprocessor.colbert_rerank import ColbertRerank
 from llama_index.core.postprocessor import LLMRerank 
 from llama_index.core import QueryBundle
+from llama_index.core.response_synthesizers import ResponseMode
+from llama_index.core.query_engine import CitationQueryEngine
+from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.query_engine import RouterQueryEngine
 
 from llama_index.core.agent import (
     StructuredPlannerAgent,
@@ -53,12 +57,26 @@ from llama_index.core.node_parser import (
 
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.node_parser import MarkdownNodeParser
-
+from llama_index.readers.docling import DoclingReader
 import nltk
 import spacy
 import uuid
+from llama_index.core.query_engine import CustomQueryEngine
+from llama_index.core.tools import QueryEngineTool
+from llama_index.core import DocumentSummaryIndex
 
 
+class LlmQueryEngine(CustomQueryEngine):
+    """Custom query engine for direct calls to the LLM model."""
+
+    llm: LLM
+    prompt: str
+
+    def custom_query(self, query_str: str):
+        llm_prompt = self.prompt.format(query=query_str)
+        llm_response = self.llm.complete(llm_prompt)
+        return str(llm_response)
+    
 class PdfFileReader(BaseReader):
     def __init__(self, pdf_images_path: str, page_chunks: bool = False):
        self.pdf_images_path = pdf_images_path
@@ -66,7 +84,7 @@ class PdfFileReader(BaseReader):
        
     def load_data(self, file, extra_info={}):
         md_content = pymupdf4llm.to_markdown(file, write_images=True, page_chunks=self.page_chunks, image_path=self.pdf_images_path)
-        # load_data returns a list of Document objects
+        # load_data returns a list of Document objects        
         nodes = []
         if self.page_chunks:
             for d in md_content:
@@ -82,8 +100,41 @@ class PdfFileReader(BaseReader):
             nodes.append(doc)
         return nodes
 
+class DocklingFileReader(BaseReader):
+    def __init__(self, pdf_images_path: str, page_chunks: bool = False):
+       self.pdf_images_path = pdf_images_path
+       self.page_chunks = page_chunks
+       
+    def load_data(self, file, extra_info={}):
+        #md_content = pymupdf4llm.to_markdown(file, write_images=True, page_chunks=self.page_chunks, image_path=self.pdf_images_path)
+        # load_data returns a list of Document objects
+        reader = DoclingReader()
 
-    
+        md_content = reader.load_data(file)
+        nodes = []
+        if self.page_chunks:
+            for d in md_content:
+                # res = mrkdown_parser.aget_nodes_from_documents(d)
+                # for n in res:
+                    # print(n)
+                doc_id = f"{d['metadata']['title']}:{d['metadata']['page']}"
+                doc = Document(text=d['text'], id_=doc_id, extra_info={**extra_info, **d['metadata']})
+                nodes.append(doc)
+        else:
+            doc_id = f"{file}"
+            doc = Document(text=md_content, id_=doc_id)                
+            nodes.append(doc)
+        return nodes
+
+direct_llm_prompt = (
+    "Given the user query, respond as best as possible following this guidelines:\n"
+    "- If the intent of the user is to get information about the abilities of the AI, respond with: "
+    "This assistant can answer questions, generate text, summarize documents, and more. \n"
+    "- If the intent of the user is harmful. Respond with: I cannot help with that. \n"
+    "- If the intent of the user is to get information outside of the context given, respond with: "
+    "I cannot help with that. Please ask something that is relevant with the documents in the context givem. \n"
+    "Query: {query}")
+
 class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
     """Multi-document Agents pack.
 
@@ -98,7 +149,8 @@ class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
         storage_dir: str,
         pdf_images_path: str,
         verbose: bool = False,
-        number_of_files: int = None,        
+        number_of_files: int = None,
+        page_chunks: bool = False,        
         **kwargs: Any,
     ) -> None:
         """Init params."""
@@ -114,8 +166,9 @@ class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
         self.agent_llm = agent_llm
         self.main_llm = main_llm
         self.number_of_files = number_of_files
+        self.page_chunks = page_chunks
         # this is for the baseline
-        
+        self.vector_index: VectorStoreIndex = None
         
         if os.path.exists(storage_dir):
             self.vector_index = load_index_from_storage(
@@ -134,48 +187,33 @@ class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
         #                                 tokenizer="colbert-ir/colbertv2.0",
         #                                 keep_retrieval_score=True,
         #                             )
-        
-        self.vector_query_engine = self.vector_index.as_query_engine(llm=agent_llm, similarity_top_k=10)
-         
-        self.vector_query_engine_reranked = self.vector_index.as_query_engine(llm=agent_llm, similarity_top_k=10,
-                                                                             node_postprocessors=[self.reranker])
-        query_engine_tools = [
-                QueryEngineTool (
-                    query_engine=self.vector_query_engine,
-                    metadata=ToolMetadata(
-                        name="vector_tool",
-                        description="Useful to answer question based on relevant documents. ",
-                    )
-                )
-        ] 
 
-        self.tool_interactive_reflection_agent_worker = FunctionCallingAgentWorker.from_tools(
-            tools=query_engine_tools, llm=agent_llm, verbose=self.verbose
-        )
-        
-        self.main_agent = ReActAgentWorker.from_tools(tools=query_engine_tools, llm=main_llm,
-             verbose=self.verbose
-        )
+        self.vector_query_engine = CitationQueryEngine.from_args(self.vector_index, llm=agent_llm, similarity_top_k=4, citation_chunk_size=512,)
+        # self.vector_query_engine = self.vector_index.as_query_engine(llm=agent_llm, similarity_top_k=4,
+        #         response_synthesizer_mode=ResponseMode.REFINE,)
+        self.vector_query_engine_reranked = CitationQueryEngine.from_args(self.vector_index, llm=agent_llm, similarity_top_k=4, citation_chunk_size=512, node_postprocessors=[self.reranker]) 
+        # self.vector_query_engine_reranked = self.vector_index.as_query_engine(llm=agent_llm, similarity_top_k=10,
+        #                                                                      node_postprocessors=[self.reranker])
+        self.build_query_tool()
+        self.build_query_router()
 
-        chat_history = [
-            ChatMessage(
-                content="You are an assistant that generates answer based on documents. Please provide the answer in John Cleese style.",
-                role=MessageRole.SYSTEM,
-            )
-        ]
-        self.top_agent = self.main_agent.as_agent(chat_history=chat_history, verbose=self.verbose)
+    def build_index(self) -> VectorStoreIndex:       
+        nltk.download('punkt_tab')
+        spacy.cli.download("en_core_web_md")
 
-    def build_index(self):        
         file_extractor={".pdf": PdfFileReader(self.pdf_images_path, page_chunks=self.page_chunks)}
+        #file_extractor={".pdf": DoclingReader()}
         config = LanguageConfig(language="english", spacy_model="en_core_web_md")
-        if self.number_of_docs != None:
+        if self.number_of_files != None:
+            
             reader = SimpleDirectoryReader(input_dir=self.docs_store_path, num_files_limit=self.number_of_files, file_extractor=file_extractor)
         else:
             reader = SimpleDirectoryReader(input_dir=self.docs_store_path, file_extractor=file_extractor)
+                
         documents = reader.load_data(show_progress=True)
-        parser = MarkdownNodeParser()
-        markdown_pages = parser.get_nodes_from_documents(documents)
-
+        response_synthesizer = get_response_synthesizer(
+            response_mode="tree_summarize", use_async=True
+        )
         sentence_splitter = SentenceSplitter(
                 chunk_size=3000,
                 chunk_overlap=200,
@@ -184,35 +222,80 @@ class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
         def id_func(i: int, doc: BaseNode) -> str:
             return f"{doc.id_}_{str(uuid.uuid4())}"
 
-
-        nltk.download('punkt_tab')
-        spacy.cli.download("en_core_web_md")
-        splitter = SemanticDoubleMergingSplitterNodeParser(
-                language_config=config,
-                initial_threshold=0.4,
-                appending_threshold=0.6,
-                merging_threshold=0.6,
-                max_chunk_size=5000,
-                merging_range=2,
-                splitter=[sentence_splitter],
-                id_func=id_func,
+        parser = MarkdownNodeParser()
+        vector_index = {}
+        for doc in documents:            
+            #markdown_pages = parser.get_nodes_from_documents([doc])
+            splitter = SemanticDoubleMergingSplitterNodeParser(
+                    language_config=config,
+                    initial_threshold=0.4,
+                    appending_threshold=0.6,
+                    merging_threshold=0.6,
+                    max_chunk_size=5000,
+                    merging_range=2,
+                    splitter=[parser, sentence_splitter],
+                    id_func=id_func,
+                )
+            nodes = splitter.get_nodes_from_documents([doc])
+            transformations=[   
+                    self.embed_model
+                ]
+            vector_index = VectorStoreIndex(nodes=nodes, transformations=transformations, show_progress=True)
+            summary_index = SummaryIndex(nodes)
+            summary_query_engine = summary_index.as_query_engine(
+                response_mode="tree_summarize", llm=self.main_llm
             )
-        nodes = splitter.get_nodes_from_documents(markdown_pages)
-        transformations=[   
-                self.embed_model
-            ]
-        vector_index = VectorStoreIndex(nodes=nodes, transformations=transformations, show_progress=True)
-        
+            summary = summary_query_engine.query(
+                "Extract a concise 1-2 line summary of this document"
+            )
+            print(f"Summary: {summary}")
         return vector_index
-   
+        
+    def build_query_tool(self):
+        self.llm_query_engine = LlmQueryEngine(
+            llm=self.main_llm, prompt=direct_llm_prompt)
+        
+
+    def build_query_router(self):
+        self.llm_tool = QueryEngineTool.from_defaults(
+            query_engine=self.llm_query_engine,
+            name="llm_query_tool",
+            description=(
+                "Useful for when the INTENT of the user isnt clear, is broad, "
+                "or when the user is asking general questions that have nothing "
+                "to do with data-intensive application. Use this tool when the other tool is not useful."
+                "Provide a clarifying question to the user to get more context."
+            ), 
+        )
+        self.vector_tool = QueryEngineTool.from_defaults(
+            query_engine=self.vector_query_engine,
+            name="vector_query_tool",
+            description=(
+                "Useful for retrieving specific context about designing Data-Intensive Applications."
+                "A data-intensive application is an application that is typically built from standard"
+                " building blocks which provide commonly needed functionality, such as storing data,"
+                " caching, search indexes, message queues, stream processing, and batch processing."
+                " These applications rely on data systems to achieve their characteristics, ""but may require a combination of tools to accomplish tasks that cannot be done alone by a single tool."
+            ), 
+        )
+        self.router_query_engine = RouterQueryEngine(
+            selector=LLMSingleSelector.from_defaults(),
+            query_engine_tools=[
+                self.llm_tool,
+                self.vector_tool,
+            ], verbose = self.verbose
+        )
+        
     def query_index(self, query: str, top_k: int = 5):
-        return self.vector_query_engine.query(str)
+        response = self.vector_query_engine.query(query)
+        result = response.response + response.get_formatted_sources()
+        return result
     
     def query_rerank(self, query: str, top_k: int = 5):                
         return self.vector_query_engine_reranked.query(query)
     
     def query_agent(self, query: str, top_k: int = 5):
-        return self.top_agent.query(query)
+        return self.router_query_engine.query(query)
     
     def process_documets(self, docs_store_path):
         file_extractor={".pdf": PdfFileReader(self.pdf_images_path)}
@@ -234,5 +317,8 @@ class MultiDocumentAssistantAgentsPack(BaseLlamaPack):
         """Run the pipeline."""
         #return self.top_agent.query(*args, **kwargs)
         #return self.vector_query_engine.query(*args, **kwargs)
-        result = self.query_rerank(args[0], top_k=5)
+        query: str = args[0]
+        print(f"Query: {query}")
+        #result = self.query_index(query, top_k=5)
+        result = self.query_agent(query, top_k=5)
         return result 
---
+++ intent_agent.py
@@ -0,0 +1,68 @@
+from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
+from assistant.multi_document_agent.personal_assitant import MultiDocumentAssistantAgentsPack
+from dotenv import load_dotenv
+load_dotenv(verbose=True, override=True)
+import os
+from langfuse.llama_index import LlamaIndexCallbackHandler
+from llama_index.core.callbacks import CallbackManager
+import uuid
+from llama_index.core import Settings
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.core.query_engine import CitationQueryEngine
+
+ollama_base_url=os.environ['OLLAMA_BASE_URL']
+pdf_images_path=os.environ['PDF_IMAGES_STORE_PATH']
+docs_store_path=os.environ['DOCS_STORE_PATH']
+langfuse_url = os.environ['LANGFUSE_BASE_URL']
+langfuse_public_key = os.environ['LANGFUSE_PUBLIC_KEY']
+langfuse_secret_key = os.environ['LANGFUSE_SECRET_KEY']
+storage_dir = os.environ['STORAGE_DIR']
+phoenix_url = os.environ['PHOENIX_URL']
+#Configure 
+session_id = str(uuid.uuid4())
+print(f"Session_id: {session_id}")
+langfuse_callback_handler = LlamaIndexCallbackHandler(
+    public_key=langfuse_public_key,
+    secret_key=langfuse_secret_key,
+    host=langfuse_url,
+    session_id=session_id,
+    debug=False,
+)
+
+from phoenix.otel import register
+
+tracer_provider = register(
+  project_name="llama-test",
+  endpoint=phoenix_url,
+  batch=True,
+  verbose=True,
+)
+
+LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
+Settings.callback_manager = CallbackManager([langfuse_callback_handler])
+print(f"Ollama Url: {ollama_base_url}")
+print(f"Langfuse Url: {langfuse_url}")
+ollama_agent = Ollama(model="llama3.2:latest", request_timeout=120.0, base_url=ollama_base_url, 
+                is_function_calling_model=True)
+embed_model = OllamaEmbedding(model_name="bge-m3", request_timeout=120.0, base_url=ollama_base_url)
+
+
+agent = MultiDocumentAssistantAgentsPack(main_llm=ollama_agent, agent_llm=ollama_agent, 
+                                         embeding_llm=embed_model, 
+                                         docs_store_path=docs_store_path, 
+                                         pdf_images_path=pdf_images_path,
+                                         storage_dir=storage_dir, verbose=True, number_of_files=1)   
+
+
+async def main():
+    result = agent.run("Give me the defenition of Data-Intensive Applications")
+    print(result)
+    print("========================================")
+    result = agent.run("Could you help me explaining the weather patterns?")
+    print(result)
+    print("========================================")
+    
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
